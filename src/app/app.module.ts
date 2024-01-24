246dbd828d584721d8f74514c3eb0581b573f8b1
Sakshi Indarkhe <indarkhesakshi1234@gmail.com>
2024-01-24 15:48:16 +0530

    Hello

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    NavbarComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
